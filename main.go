644cf35546e6dcc7fcef4c02777241cb63582e65
jinho hong <tpdleps@gmail.com>
2020-04-11 17:01:34 +0900

    Handler interface example

--- main.go
+++ main.go
@@ -5,10 +5,22 @@ import (
 	"net/http"
 )
 
+type fooHandler struct {
+}
+
+func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello foo")
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello World")
 	})
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello bar")
+	})
+
+	http.Handle("/foo", &fooHandler{})
 
 	http.ListenAndServe(":3000", nil)
 }
