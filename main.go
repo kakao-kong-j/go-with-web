f67aae85a130be9f803341dc3d07f569e8d13b09
jinho hong <tpdleps@gmail.com>
2020-04-11 16:46:39 +0900

    basic http

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hi")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	})
+
+	http.ListenAndServe(":3000", nil)
 }
