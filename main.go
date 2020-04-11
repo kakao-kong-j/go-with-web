188838bd1b29c22ade0b7b2632e69e34f2d5b28e
jinho hong <tpdleps@gmail.com>
2020-04-11 17:57:56 +0900

    add response

--- main.go
+++ main.go
@@ -1,18 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
+
+type User struct {
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
 
 type fooHandler struct {
 }
 
 func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hello foo")
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Bad Request:", err)
+		return
+	}
+	user.CreatedAt = time.Now()
+
+	data, _ := json.Marshal(user)
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, string(data))
+
 }
 func barHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello bar")
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "world"
+	}
+	fmt.Fprint(w, "Hello ", name, "!")
 }
 func main() {
 	mux := http.NewServeMux()
